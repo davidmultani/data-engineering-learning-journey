f335e4733f78bfb6d08c6ddfe415a07f0f21ab2b
davidmultani <davidmultani97@gmail.com>
2026-03-20 15:57:11 +0530

    eval() and query()

--- pandas/date_and_time_in_pyhon.py
+++ pandas/date_and_time_in_pyhon.py
@@ -1,3 +1,5 @@
+import timeit
+import numexpr
 import seaborn as sns
 import matplotlib.pyplot as plt
 import yfinance as yf
@@ -298,7 +300,142 @@ data = pd.DataFrame({
     'one-year rolling mean': rolling.mean(),
     'one-year rolling std': rolling.std()
 })
-ax = data.plot(style=['-', '--', ':'])
-ax.lines[0].set_alpha(0.3)
-plt.show()
+# ax = data.plot(style=['-', '--', ':'])
+# ax.lines[0].set_alpha(0.3)
+# plt.show()
+print()
+
+### High-Performance Pandas: eval() and query() ###
+# version 0.13 (released January 2014),
+# Pandas includes some experimental tools
+# that allow you to directly access C-speed operations
+# without costly allocation of intermediate arrays.
+# These are the eval() and query() functions,
+# which rely on the Numexpr package.
+
+# For example, consider the following expression:
+# mask = (x > 0.5) & (y < 0.5)
+
+# NumPy evaluates each subexpression,
+# This is roughly equivalent to the following:
+# tmp1 = (x > 0.5)
+# tmp2 = (y < 0.5)
+# mask = tmp1 & tmp2
+
+# Every intermediate step is explicitly allocated in memory.
+# If the x and y arrays are very large,
+# This can lead to significant memory and computational overhead.
+
+# The Numexpr library gives -
+# It provides the ability to compute this type of compound expression -
+# element by element, without the need to allocate full intermediate arrays.
+
+# import numexpr
+# pandas.eval()
+# The eval() function -
+# Pandas uses string expressions to efficiently compute operations using DataFrames.
+nrows, ncolumns = 1000000, 100
+df1, df2, df3, df4 = [
+    pd.DataFrame(np.random.rand(nrows, ncolumns))
+    for _ in range(4)
+]
+time = timeit.timeit(lambda: df1+df2+df3+df4, number=10)
+print(time)  # 1.0789531249999995
+
+time = timeit.timeit(lambda: pd.eval('df1+df2+df3+df4'), number=10)
+print(time)  # 0.5356571250000002
+print()
+
+# The eval() version of this expression is about 50% faster (and uses much less memory),
+# memory is saved by not creating multiple temporary arrays.
+#
+# Normal pandas -
+#       temp1 = df1 + df2
+#       temp2 = df3 + df4
+#       result = temp1 * temp2
+#
+# pd.eval() -
+#       pd.eval('(df1 + df2) * (df3 + df4)')
+#
+# eval() version will only perform faster if dataset is huge and have millions of records.
+# For smaller dataset, the addition in pandas is already vectorized,efficient & fast.
+#
+# Reason is Overhead of pd.eval().eval() does extra work like -
+# - parses the string 'df1+df2+df3+df4'
+# - builds an expression tree
+# - decides execution engine (numexpr or python)
+# - manages evaluation context.
+
+# It will only work if numpexpr is already installed
+# Data is large enough
+# Expression is suitable
+
+# Operations supported by pd.eval() -
+
+# Arithmetic operators - pd.eval() supports all arithmetic operators.
+#   result1 = -df1 * df2 / (df3 + df4) - df5
+#   result2 = pd.eval('-df1 * df2 / (df3 + df4) - df5')
+
+# Comparison operators - pd.eval() supports all comparison operators.
+#   result1 = (df1 < df2) & (df2 <= df3) & (df3 != df4)
+#   result2 = pd.eval('df1 < df2 <= df3 != df4')
+
+# Bitwise operators - pd.eval() supports the & and | bitwise operators
+#   result1 = (df1 < 0.5) & (df2 < 0.5) | (df3 < df4)
+#   result2 = pd.eval('(df1 < 0.5) & (df2 < 0.5) | (df3 < df4)')
+
+### DataFrame.eval() for Column-Wise Operations ###
+df = pd.DataFrame(np.random.rand(1000, 3), columns=['A', 'B', 'C'])
+
+# Using pd.eval()
+result1 = (df['A']+df['B']+df['C'])
+result2 = pd.eval("df.A + df.B + df.C")
+print(np.allclose(result1, result2))  # True means both are equal
+print()
+
+# Using DataFrame.eval()
+result3 = df.eval('A + B + C')
+print(np.allclose(result1, result3))  # True means both are equal
+print()
+# Here, we can use column names as variables within the evaluated expression.
+
+# Assignment in DataFrame.eval()
+# DataFrame.eval() also allows assignment to any columns.
+# We will create a new column and in that column we will store A + B + C.
+df.eval('D = A + B + C', inplace=True)
+print(df.head())
+print()
+# In similar way, any existing columns can be modified.
+
+# Local variables in DataFrame.eval()
+# The DataFrame.eval() method supports an additional syntax -
+# that lets it work with local Python variables.
+column_name = df.mean(1)
+result1 = df['A'] + column_name
+result2 = df.eval('A + @column_name')
+# True  # all values are approximately equal.
+print(np.allclose(result1, result2))
+print()
+
+# The @ character here marks a variable name rather than a column name, and lets you
+# efficiently evaluate expressions involving the two “namespaces”:
+# - the namespace of columns, and the namespace of Python objects.
+# Notice that this @ character is only supported by the DataFrame.eval() method,
+# not by the pandas.eval() function,because
+# - the pandas.eval() function only has access to the one (Python) namespace.
+
+### DataFrame.query() Method ###
+# The DataFrame has another method based on evaluated strings,
+# called the query() method.
+result1 = df[(df.A < 5) & (df.B < 10)]
+result2 = df.query("A < 10 and B <10")
+# It is much easier to read and understand.
+print(np.allclose(result1, result2))  # True
+print()
+
+# The query() method also accepts the @ flag to mark local variables.
+mean = df['C'].mean()
+result1 = df[(df.A < mean) & (df.B < mean)]
+result2 = df.query("A < @mean and B < @mean")
+print(np.allclose(result1, result2))  # True
 print()
